1eed3bb98e6fb0426dbcbb17808d6438a4468794
VP <999helper@gmail.com>
2026-08-23 22:45:53 +0530

    Favicon bug fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,11 @@ export default function FoodsLayout({
   return (
     <html lang="en" className={geist.variable}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <OrganizationJsonLd />
       </head>
       <body className="flex flex-col min-h-screen bg-background">
